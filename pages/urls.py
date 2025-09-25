32bd591ed54eddf050c9a25e4a9e76c26a0002ad
subin kim <1008sb354@gmail.com>
2025-09-25 18:27:53 +0900

    feat: page url 추가

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import PageDetailBySlugView
 
 urlpatterns = [
-    path('pages/<slug:slug>/', PageDetailBySlugView.as_view(), name='page-detail-slug')
+    path('page/<slug:slug>', PageDetailBySlugView.as_view(), name='page-detail-slug')
 ]
